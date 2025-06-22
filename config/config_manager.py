61298500a6a57f897fc255251c85c530001059e6
White Torn <FaustusTorn@protonmail.com>
2025-06-22 13:28:28 +0400

    Mark II - 2.0.1 added error handling to config manager

--- config/config_manager.py
+++ config/config_manager.py
@@ -7,14 +7,40 @@ class ConfigManager:
         self.config_path = os.path.join(os.path.dirname(__file__), 'sources.json')
 
     def load_config(self):
-        with open(self.config_path, 'r') as file:
-            self.config = json.load(file)
+        """
+        Loads the config from the config file.
+        If the file does not exist, error will be raised.
+        If the file is not a valid JSON file, error will be raised.
+        """
+        try:
+            with open(self.config_path, 'r') as file:
+                self.config = json.load(file)
+        except FileNotFoundError:
+            print(f"Error: The file {self.config_path} does not exist.")
+            self.config = {}
+        except json.JSONDecodeError:
+            print(f"Error: The file {self.config_path} is not a valid JSON file.")
+            self.config = {}
+        except Exception as e:
+            print(f"Error: An unexpected error occurred while loading the config file: {e}")
+            self.config = {}
             
     def print_config(self):
-        with open(self.config_path, 'r') as file:
-            self.config = json.load(file)
-            pretty = json.dumps(self.config, indent=4)
-            print(pretty)
+        try:
+            with open(self.config_path, 'r') as file:
+                self.config = json.load(file) # you can't print the config pretty because it has no indent.
+                pretty = json.dumps(self.config, indent=4)
+                print(pretty)
+        except FileNotFoundError:
+            print(f"Error: The file {self.config_path} does not exist.")
+            self.config = {}
+        except json.JSONDecodeError:
+            print(f"Error: The file {self.config_path} is not a valid JSON file.")
+            self.config = {}
+        except Exception as e:
+            print(f"Error: An unexpected error occurred while printing the config file: {e}")
+            self.config = {}
+
 
 config_manager = ConfigManager()
 config_manager.print_config()
